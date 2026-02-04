95a3cf35d4cd022a2fe66f90b249c0e79b53fc90
clstalys <lysterine3@gmail.com>
2026-02-04 20:10:07 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_124423_LysiaCallysta/week01/GameStore.kt
+++ src/oop_124423_LysiaCallysta/week01/GameStore.kt
@@ -2,12 +2,22 @@ package oop_124423_LysiaCallysta.week01
 
 fun main() {
     val gameTitle: String = "Tes Lys"
-    val price: Int = 15
+    val price: Int = 15000
 
     println("Game Title: $gameTitle")
     println("Price: $price")
 
     println("Discount: ${calculateDiscount(price)}")
+
+    val finalPrice = calculateDiscount(price)
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) price * 80 / 100 else price * 90 / 100
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("RECEIPT")
+    println("Title: $title")
+    println("Final Price: $finalPrice")
+}
