f90c6d80195dafcdb69f4d0929d7c8e2da30bdb8
clstalys <lysterine3@gmail.com>
2026-02-04 16:26:00 +0700

    week01: refactor to use val and string templates

--- src/oop_124423_LysiaCallysta/week01/GradingSystem.kt
+++ src/oop_124423_LysiaCallysta/week01/GradingSystem.kt
@@ -1,9 +1,9 @@
 package oop_124423_LysiaCallysta.week01
 
 fun main (){
-    var name: String = "Lysia Callysta"
-    var score: Int = 80
+    val name: String = "Lysia Callysta"
+    val score: Int = 80
 
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
 
