9ec276c76b613aca4da13602c2a9be2fa181dfc2
clstalys <lysterine3@gmail.com>
2026-02-04 19:51:50 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_124423_LysiaCallysta/week01/GameStore.kt
@@ -0,0 +1,9 @@
+package oop_124423_LysiaCallysta.week01
+
+fun main() {
+    val gameTitle: String = "Tes Lys"
+    val price: Int = 15
+
+    println("Game Title: $gameTitle")
+    println("Price: $price")
+}
