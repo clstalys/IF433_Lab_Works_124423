204c9b48102d600aba5421fc5714cd29ee3b3d49
clstalys <lysterine3@gmail.com>
2026-02-11 16:14:21 +0700

    week02: (task 1) implement library input system

--- src/oop_124423_LysiaCallysta/week02/Loan.kt
+++ src/oop_124423_LysiaCallysta/week02/Loan.kt
@@ -1,5 +1,7 @@
 package oop_124423_LysiaCallysta.week02
 
+import java.util.Scanner
+
 class Loan (val bookTitle: String, val borrower: String, var loanDuration: Int = 1) {
 
     fun calculateFine(): Int{
@@ -7,4 +9,32 @@ class Loan (val bookTitle: String, val borrower: String, var loanDuration: Int =
             (loanDuration - 3) * 2000
         } else {0}
     }
+}
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    println("--- LIBRARY FINE SYSTEM ---")
+
+    print("Masukkan Judul Buku: ")
+    val bookTitle = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukkan Durasi Lama Pinjam: ")
+    var loadDuration = scanner.nextInt()
+
+    scanner.nextLine()
+
+    if (loadDuration < 0){
+        loadDuration = 1
+    }
+
+    val loan = Loan (bookTitle, borrower, loadDuration)
+
+    println("Judul: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Durasi: ${loan.loanDuration}")
+    println("Total Denda: Rp ${loan.calculateFine()}")
 }
