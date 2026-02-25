28c01564f73b9f0b39aa93aa5fd68b32f27cb8d5
clstalys <lysterine3@gmail.com>
2026-02-25 15:11:51 +0700

    week04: (task 1)create ElectricCar class with final override

---
+++ src/oop_124423_LysiaCallysta/week04/ElectricCar.kt
@@ -0,0 +1,7 @@
+package oop_124423_LysiaCallysta.week04
+
+class ElectricCar(brand: String, numberOfDoors: Int, val batteryCapacity: Int) :Car(brand, numberOfDoors) {
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
+}
