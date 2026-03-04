9380fc2cd771148184f25eb91c8868aa63f311c7
clstalys <lysterine3@gmail.com>
2026-03-04 15:09:22 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_124423_LysiaCallysta/week05/MathHelper.kt
@@ -0,0 +1,13 @@
+package oop_124423_LysiaCallysta.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int{
+        return sisi * sisi
+    }
+    fun hitungLuas(panjang: Int, lebar: Int): Int{
+        return panjang * lebar
+    }
+    fun hitungLuas(jariJari: Double): Int{
+        return 3.14 * jariJari * jariJari
+    }
+}
