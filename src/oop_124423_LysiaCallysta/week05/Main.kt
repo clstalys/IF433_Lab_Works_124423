71e1691a67b129cc188b2cd0d415294e01a3c752
clstalys <lysterine3@gmail.com>
2026-03-04 15:02:14 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_124423_LysiaCallysta/week05/Main.kt
@@ -0,0 +1,27 @@
+package oop_124423_LysiaCallysta.week05
+
+fun Main() {
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai){
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn}")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdetesi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("------------------------")
+    }
+
+
+}
