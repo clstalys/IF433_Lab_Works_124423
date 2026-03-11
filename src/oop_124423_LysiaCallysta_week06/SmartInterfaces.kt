5839ffa50aa93da75b461690ea9d9f1cbc0b8650
clstalys <lysterine3@gmail.com>
2026-03-11 15:27:41 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_124423_LysiaCallysta_week06/SmartInterfaces.kt
@@ -0,0 +1,18 @@
+package oop_124423_LysiaCallysta_week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
