66c14bad849022d71ecca89e6da824b5aae71247
clstalys <lysterine3@gmail.com>
2026-03-11 15:04:56 +0700

    week06: create Button class implementing Clickable

---
+++ src/oop_124423_LysiaCallysta_week06/Button.kt
@@ -0,0 +1,7 @@
+package oop_124423_LysiaCallysta_week06
+
+class Button(override val name:String) : Clickable {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!)
+    }
+}
