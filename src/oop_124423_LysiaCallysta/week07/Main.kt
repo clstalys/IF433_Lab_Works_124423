fd0c4451aaf9fe0ef433744cdb6f2f360c2ff0ac
clstalys <lysterine3@gmail.com>
2026-03-25 15:43:50 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_124423_LysiaCallysta/week07/Main.kt
+++ src/oop_124423_LysiaCallysta/week07/Main.kt
@@ -73,4 +73,16 @@ fun main() {
     println("Damage: ${starterWeapon.item.damage}")
     println("Rarity: ${starterWeapon.item.rarity}")
     println("Durability: ${starterWeapon.durability}")
+
+    println("\n=== TEST IMMUTABILITY & EVENT FLOW ===")
+
+    val upgradedItem = starterWeapon.item.copy(damage = 25)
+
+    processEvent(BattleState.SafeZone)
+
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+
+    processEvent(BattleState.LootDropped(upgradedItem))
+
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
