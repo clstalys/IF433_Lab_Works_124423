496f0c92a1bf3b4ea9c604d4ca285252d3dbdbfa
clstalys <lysterine3@gmail.com>
2026-03-25 15:17:21 +0700

    week07: create ApiResponse sealed class

--- src/oop_124423_LysiaCallysta/week07/SystemStates.kt
+++ src/oop_124423_LysiaCallysta/week07/SystemStates.kt
@@ -1,5 +1,12 @@
 package oop_124423_LysiaCallysta.week07
 
 enum class SystemStates {
-    STARTING, RUNNING, STOPPED
+    STARTING, RUNNING, STOPPED;
+
+    sealed class ApiResponse{
+        data class Success(val data: String) : ApiResponse()
+        data class Error(val error: String) : ApiResponse()
+
+        object Loading : ApiResponse()
+    }
 }
