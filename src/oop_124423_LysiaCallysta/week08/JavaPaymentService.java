15cc23964615b3bbb5ea9c2eb4fd32fcd09f9651
clstalys <lysterine3@gmail.com>
2026-04-15 15:39:40 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_124423_LysiaCallysta/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_124423_LysiaCallysta.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
