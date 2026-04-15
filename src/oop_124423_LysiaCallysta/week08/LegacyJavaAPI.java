0da504c1c025a3083434a36196b108a9f6be1371
clstalys <lysterine3@gmail.com>
2026-04-15 15:24:25 +0700

    week08: implement jaav interop using !!

--- src/oop_124423_LysiaCallysta/week08/LegacyJavaAPI.java
+++ src/oop_124423_LysiaCallysta/week08/LegacyJavaAPI.java
@@ -2,6 +2,6 @@ package oop_124423_LysiaCallysta.week08;
 
 public class LegacyJavaAPI {
     public static String fetchServerStatus(){
-        return "SERVER_ONLINE_200_OK"
+        return "SERVER_ONLINE_200_OK";
     }
 }
--- src/oop_124423_LysiaCallysta/week08/main.kt
+++ src/oop_124423_LysiaCallysta/week08/main.kt
@@ -56,4 +56,10 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("===TEST JAVA INTEROP===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
