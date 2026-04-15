f8e5d85520a49477b3e51564d802c4c0fa2d3062
clstalys <lysterine3@gmail.com>
2026-04-15 14:48:50 +0700

    week08: implement smart cast in function

---
+++ src/oop_124423_LysiaCallysta/week08/NotificationService.kt
@@ -0,0 +1,17 @@
+package oop_124423_LysiaCallysta.week08
+
+class NotificationService {
+
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+
+        if (user.email != null) {
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
