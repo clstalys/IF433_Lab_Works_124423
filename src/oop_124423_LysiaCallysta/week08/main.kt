90af5eeec66599235b12e5400ced6a5547f4300a
clstalys <lysterine3@gmail.com>
2026-04-15 14:57:32 +0700

    week08:implement safe call with let block

--- src/oop_124423_LysiaCallysta/week08/main.kt
+++ src/oop_124423_LysiaCallysta/week08/main.kt
@@ -1,10 +1,22 @@
 package oop_124423_LysiaCallysta.week08
 
-fun main {
+fun main() {
     println("===TEST SAFE CALLS & ELVIS===")
     val emptyOrder = Order(null, null)
 
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota tidak Diketahui"
 
     println("Tujuan Pengiriman $destination")
+
+
+    println("===TEST LET BLOCK===")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Rp$price, Pajak Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
+
 }
