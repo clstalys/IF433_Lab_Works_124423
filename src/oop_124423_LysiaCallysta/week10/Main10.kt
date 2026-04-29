a91d1f5c6c286044b0759d0ebaeeaa23f3df323c
clstalys <lysterine3@gmail.com>
2026-04-29 10:30:55 +0700

    week10: test generic functions with Double and String

--- src/oop_124423_LysiaCallysta/week10/Main10.kt
+++ src/oop_124423_LysiaCallysta/week10/Main10.kt
@@ -12,4 +12,10 @@ fun main(){
     val itemPrice = PairBox("Bitcoin", 65000)
 
     println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
+
+    println("===TEST GENERIC FUNCTIONS===")
+    printData(3.14)
+
+    val result = processData("Stable Coin")
+    println("Hasil proses: $result")
 }
