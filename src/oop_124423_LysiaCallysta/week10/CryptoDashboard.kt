79462aff04127b3fd3bb788d21135c8d06e68478
clstalys <lysterine3@gmail.com>
2026-04-29 11:02:02 +0700

    week10: (task) populate coin repository

--- src/oop_124423_LysiaCallysta/week10/CryptoDashboard.kt
+++ src/oop_124423_LysiaCallysta/week10/CryptoDashboard.kt
@@ -2,4 +2,10 @@ package oop_124423_LysiaCallysta.week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 10000.0))
+    coinRepo.add(Coin("ETH", 5000.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+
+    println(coinRepo.getAll())
 }
