054fa112bfbca3e5d2dcda75bf55c8c81f7370c1
clstalys <lysterine3@gmail.com>
2026-05-06 10:34:53 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_124423_LysiaCallysta/week11/SmartHomeModels.kt
@@ -0,0 +1,6 @@
+package oop_124423_LysiaCallysta.week11
+
+import jdk.jfr.Category
+
+data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0) {
+}
