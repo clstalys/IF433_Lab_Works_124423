cbdafe39e9d9ff0db9b4488b2106d38a2537e49f
clstalys <lysterine3@gmail.com>
2026-05-06 10:53:44 +0700

    week11: (task) create extension function for SMartDevice

--- src/oop_124423_LysiaCallysta/week11/SmartHomeModels.kt
+++ src/oop_124423_LysiaCallysta/week11/SmartHomeModels.kt
@@ -4,3 +4,9 @@ import jdk.jfr.Category
 
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0) {
 }
+
+fun SmartDevice.diagnose(): String {
+    return "[DIAGNOSTIK] $name | Kategori: $category | " +
+            "Status: ${if (isOnline) "Online" else "Offline"} | " +
+            "Daya: $powerLoad Watt"
+}
