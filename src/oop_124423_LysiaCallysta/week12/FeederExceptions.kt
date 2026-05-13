92dfa974c70015ef0e39547339b24e50cb48f8bc
clstalys <lysterine3@gmail.com>
2026-05-13 10:43:29 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_124423_LysiaCallysta/week12/FeederExceptions.kt
+++ src/oop_124423_LysiaCallysta/week12/FeederExceptions.kt
@@ -1,4 +1,9 @@
 package oop_124423_LysiaCallysta.week12
 
-sealed class FeederExceptions (msg: String): Exception(msg) {
-}
+sealed class FeederException(msg: String) : Exception(msg)
+
+class FoodEmptyException(requested: Int, available: Int) :
+    FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+class DispenserJamException :
+    FeederException("Wadah dispenser tersangkut/macet!")
