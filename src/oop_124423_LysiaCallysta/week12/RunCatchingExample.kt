f91d542f688f95de9a16c80934eb3bf3b75ef6c4
clstalys <lysterine3@gmail.com>
2026-05-13 10:39:23 +0700

    week12: handle result using getorelse and recover

--- src/oop_124423_LysiaCallysta/week12/RunCatchingExample.kt
+++ src/oop_124423_LysiaCallysta/week12/RunCatchingExample.kt
@@ -5,4 +5,10 @@ fun main(){
     val result: Result<Int> = runCatching {
         "42X".toInt()
     }
+
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
+
+    val recovered = result.recover { 0 }.getOrElse { -1 }
+    println("Recoverd Value (getOrElse): $recovered")
 }
