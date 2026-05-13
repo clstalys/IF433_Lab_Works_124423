13394c0ee47636ccafb8d576fa7f22e690926f07
clstalys <lysterine3@gmail.com>
2026-05-13 10:24:36 +0700

    week12: create InsufficientFunds exception

--- src/oop_124423_LysiaCallysta/week12/CustomExceptions.kt
+++ src/oop_124423_LysiaCallysta/week12/CustomExceptions.kt
@@ -2,3 +2,5 @@ package oop_124423_LysiaCallysta.week12
 
 class InsufficientFundsException(val amount: Double, val balance: Double) : Exception("Attempted $amount, balance: $balance") {
 }
+
+//udh kepush di commit 2 krna sblmnya lupa commit...
