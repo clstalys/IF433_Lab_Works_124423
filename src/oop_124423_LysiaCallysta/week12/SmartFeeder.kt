19a1d71ae5f681c1ea8a69cb8cd585445bd9ef1f
clstalys <lysterine3@gmail.com>
2026-05-13 10:51:48 +0700

    week12: handle feeding execution with multiple catch

--- src/oop_124423_LysiaCallysta/week12/SmartFeeder.kt
+++ src/oop_124423_LysiaCallysta/week12/SmartFeeder.kt
@@ -19,4 +19,19 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main(){
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+        println("Sisa stok: $currentKibbleStock gr")
+    } catch (e: DispenserJamException) {
+        println("ERROR DISPENSER: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("ERROR STOK: ${e.message}")
+    } catch (e: Exception) {
+        println("ERROR UMUM: ${e.message}")
+    }
 }
