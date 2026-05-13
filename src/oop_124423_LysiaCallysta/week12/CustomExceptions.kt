92173c398f9eff8e398663c468444b082e52b851
clstalys <lysterine3@gmail.com>
2026-05-13 10:26:31 +0700

    week12: implement throw keyword for validation

--- src/oop_124423_LysiaCallysta/week12/CustomExceptions.kt
+++ src/oop_124423_LysiaCallysta/week12/CustomExceptions.kt
@@ -4,3 +4,16 @@ class InsufficientFundsException(val amount: Double, val balance: Double) : Exce
 }
 
 //udh kepush di commit 2 krna sblmnya lupa commit...
+
+class BankAccount(var balance: Double) : Exception("Bank account balance $balance"){
+    fun withdraw(amount: Double) {
+        if (amount < 0){
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if (amount > balance){
+            throw InsufficientFundsException(amount, balance)
+        }
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
