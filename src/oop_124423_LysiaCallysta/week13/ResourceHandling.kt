55cd456bca1c1de484b08a9e4d6d349a79b5d702
clstalys <lysterine3@gmail.com>
2026-05-20 10:31:55 +0700

    week13: implemeny printWriter with safe use block

--- src/oop_124423_LysiaCallysta/week13/ResourceHandling.kt
+++ src/oop_124423_LysiaCallysta/week13/ResourceHandling.kt
@@ -11,4 +11,14 @@ fun main(){
 
     writer.close()
     println("Proses penulisan unsafe selesai")
+
+    println("===SAFE RESOURCE HANDLING===")
+    val safeFile = File("safe_logs.txt")
+
+    safeFile.printWriter().use { out ->
+        for (i in 1..100){
+            out.println("Safe Log ENtry #$i: System status OK.")
+        }
+    }
+    println("100 baris log berhasil di generate dengan sangat aman")
 }
